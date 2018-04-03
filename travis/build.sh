4006cf76930d4409850b58783cecc819ea737392
Khai Do <zaro0508@gmail.com>
2018-04-03 16:44:44 -0700

    Setup travis CI Build
    Setup travis to run tests on PR on master and stable-* branches.
    Project is not setup to generate archive builds so those have been
    commented out and nothing gets deployed.

---
+++ travis/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
+    echo "Build on PR"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"CardiorespiratoryFitness/CardiorespiratoryFitness.xcodeproj"  scheme:"CRFTestApp"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+    echo "Build on merge to master"
+#    bundle exec fastlane ci_archive scheme:"CRFTestApp" export_method:"app-store" project:"CardiorespiratoryFitness/CardiorespiratoryFitness.xcodeproj" 
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
+    echo "Build on stable branch"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"CardiorespiratoryFitness/CardiorespiratoryFitness.xcodeproj"  scheme:"CRFTestApp"
+#    bundle exec fastlane beta scheme:"CRFTestApp" export_method:"app-store" project:"CardiorespiratoryFitness/CardiorespiratoryFitness.xcodeproj" 
+fi
+exit $?
