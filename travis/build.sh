3cea72aa341ac13a11d280842d3bcd0edf391e4b
syoung-smallwisdom <shannon.young@sagebase.org>
2019-04-22 20:20:53 -0700

    Attemp #3 at fixing the build

--- travis/build.sh
+++ travis/build.sh
@@ -2,19 +2,19 @@
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     echo "Build on PR"
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"CRFSampleApp/CRFSampleApp.xcodeproj"  scheme:"CRFSampleApp"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"CRFSampleApp"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     echo "Build on merge to master"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"CRFSampleApp/CRFSampleApp.xcodeproj"  scheme:"CRFSampleApp"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"CRFSampleApp"
     bundle exec fastlane keychains
     bundle exec fastlane certificates
-    bundle exec fastlane ci_archive scheme:"CRFSampleApp" export_method:"app-store" project:"CRFSampleApp/CRFSampleApp.xcodeproj"
+    # bundle exec fastlane ci_archive scheme:"CRFSampleApp" export_method:"app-store"
     # bundle exec fastlane ci_archive scheme:"CRFHeartRate" export_method:"app-store" project:"CRFHeartRate/CRFHeartRate.xcodeproj"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     echo "Build on stable branch"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"CRFSampleApp/CRFSampleApp.xcodeproj"  scheme:"CRFSampleApp"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"CRFSampleApp"
     bundle exec fastlane keychains
     bundle exec fastlane certificates
     # bundle exec fastlane bump_build scheme:"CRFSampleApp" export_method:"app-store" project:"CRFSampleApp/CRFSampleApp.xcodeproj"
