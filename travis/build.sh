7055337da9521a6f5f51c5b7f691e4593cb446ec
syoung-smallwisdom <shannon.young@sagebase.org>
2019-05-01 16:56:51 -0700

    Updates to travis

--- travis/build.sh
+++ travis/build.sh
@@ -10,14 +10,15 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
     bundle exec fastlane keychains
     bundle exec fastlane certificates
     # bundle exec fastlane ci_archive scheme:"CRFSampleApp" export_method:"app-store"
-    # bundle exec fastlane ci_archive scheme:"CRFHeartRate" export_method:"app-store" project:"CRFHeartRate/CRFHeartRate.xcodeproj"
+    bundle exec fastlane ci_archive scheme:"CRFHeartRate" export_method:"app-store" project:"CRFHeartRate/CRFHeartRate.xcodeproj"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     echo "Build on stable branch"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"CRFSampleApp"
+    bundle exec fastlane bump_all
     bundle exec fastlane keychains
     bundle exec fastlane certificates
     # bundle exec fastlane bump_build scheme:"CRFSampleApp" export_method:"app-store" project:"CRFSampleApp/CRFSampleApp.xcodeproj"
-    # bundle exec fastlane beta scheme:"CRFHeartRate" export_method:"app-store" project:"CRFHeartRate/CRFHeartRate.xcodeproj"
+    bundle exec fastlane beta scheme:"CRFHeartRate" export_method:"app-store" project:"CRFHeartRate/CRFHeartRate.xcodeproj"
 fi
 exit $?
