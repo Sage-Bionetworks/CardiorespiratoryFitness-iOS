556b9d00e7e1721b33fbf60ed61e02dd1ba192ae
Shannon Young <shannon.young@sagebase.org>
2021-02-15 09:15:15 -0800

    Fix comments in response to PR

--- CardiorespiratoryFitness/Bundle+module.swift
+++ CardiorespiratoryFitness/Bundle+module.swift
@@ -2,7 +2,7 @@
 //  Bundle+module.swift
 //  Research
 //
-//  Copyright © 2020 Sage Bionetworks. All rights reserved.
+//  Copyright © 2021 Sage Bionetworks. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
